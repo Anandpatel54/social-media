e4d4a8cfd167b0606be7aa3af69001ba6b037b8b
Anand patel <panand054@gmail.com>
2024-09-26 16:05:27 +0530

    commentSchema done

---
+++ backend/models/commentModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  post: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+});
+export const Comment = mongoose.model('Comment', commentSchema);
--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -9,9 +9,7 @@ const postSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  author: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  ],
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
 });
