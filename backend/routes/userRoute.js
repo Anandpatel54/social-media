fb727d3ebfe30612dc6befae39d348bd860b1d16
Anand patel <panand054@gmail.com>
2024-10-04 13:18:46 +0530

    userRoute part done

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {
+  register,
+  login,
+  logout,
+  getProfile,
+  editProfile,
+  getSuggestedUsers,
+  followOrUnfollow,
+} from "../controllers/userController.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/logout", logout);
+router.get("/:id/profile", isAuthenticated, getProfile);
+router.post(
+  "/profile/edit",
+  isAuthenticated,
+  upload.single("profilePicture"),
+  editProfile
+);
+router.get("/suggested", isAuthenticated, getSuggestedUsers);
+router.post("/followorunfollow/:id", isAuthenticated, followOrUnfollow);
