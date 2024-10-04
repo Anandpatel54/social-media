572d8ee2f77ba2ef07f8aef2256254b87bebf361
Anand patel <panand054@gmail.com>
2024-10-04 13:44:21 +0530

    All part done

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/userRoute.js";
 dotenv.config({});
 
 const app = express();
@@ -16,17 +17,17 @@ app.get("/", (req, res) => {
 });
 
 // Middlewares
-app.use(
-  cors({
-    origin: "http://localhost:5153",
-    credentials: true,
-  })
-);
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+const corsOptions = {
+  origin: "http://localhost:5153",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 // Route
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, () => {
   connectDB();
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -9,6 +9,7 @@ import {
   followOrUnfollow,
 } from "../controllers/userController.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
+import upload from "../middlewares/multer.js";
 
 const router = express.Router();
 
@@ -16,11 +17,8 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
 router.get("/:id/profile", isAuthenticated, getProfile);
-router.post(
-  "/profile/edit",
-  isAuthenticated,
-  upload.single("profilePicture"),
-  editProfile
-);
+router.post("/profile/edit", isAuthenticated,  upload.single('profilePicture'), editProfile);
 router.get("/suggested", isAuthenticated, getSuggestedUsers);
 router.post("/followorunfollow/:id", isAuthenticated, followOrUnfollow);
+
+export default router;
