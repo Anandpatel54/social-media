bae1f4ce14f4d0db41a4ce1b257e82e82b2a3487
Anand patel <panand054@gmail.com>
2024-10-09 15:29:30 +0530

    some part done

--- frontend/src/components/Feed/Feed.jsx
+++ frontend/src/components/Feed/Feed.jsx
@@ -1,9 +1,9 @@
-import Post from "../Post/Post";
+import Posts from "../Post/Posts";
 
 const Feed = () => {
   return (
     <div className="flex-1 my-8 flex flex-col items-center pl-[20%]">
-      <Post />
+      <Posts />
     </div>
   );
 };
--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,10 +1,91 @@
+import { Dialog, DialogTrigger, DialogContent } from "../ui/dialog";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Bookmark, MessageCircle, MoreHorizontal, Send } from "lucide-react";
+import { Button } from "../ui/button";
+import { FaRegHeart } from "react-icons/fa";
+import CommentDialog from "../page/CommentDialog";
+import { useState } from "react";
 
 const Post = () => {
-  return (
-    <div>
-        post
-    </div>
-  )
-}
+  const [text, setText] = useState("");
 
-export default Post
+  const changeeventHandler = (e) => {
+    const inputText = e.target.value;
+    if (inputText.trim()) {
+      setText(inputText);
+    } else {
+      setText("");
+    }
+  };
+  return (
+    <div className="my-8 w-full max-w-sm mx-auto">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <Avatar>
+            <AvatarImage src="" alt="post_image" />
+            <AvatarFallback>Cn</AvatarFallback>
+          </Avatar>
+          <h1>username</h1>
+        </div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <MoreHorizontal className="cursor-pointer" />
+          </DialogTrigger>
+          <DialogContent className="flex flex-col items-center text-sm text-center">
+            <Button
+              variant="ghost"
+              className="cursor-pointer w-fit text-[#ED4956] font-bold"
+            >
+              Unfollow
+            </Button>
+            <Button variant="ghost" className="cursor-pointer w-fit">
+              Add to favorites
+            </Button>
+            <Button variant="ghost" className="cursor-pointer w-fit">
+              Delete
+            </Button>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <img
+        className="rounded-md my-2 w-full aspect-square object-cover"
+        src="https://images.unsplash.com/photo-1726856667612-5252bd459a69?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8"
+        alt="post_img"
+      />
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <FaRegHeart
+            size={"22px"}
+            className="cursor-pointer hover: text-gray-600"
+          />
+          <MessageCircle className="cursor-pointer hover: text-gray-600" />
+          <Send size={"22px"} className="cursor-pointer hover: text-gray-600" />
+        </div>
+        <Bookmark
+          size={"22px"}
+          className="cursor-pointer hover: text-gray-600"
+        />
+      </div>
+      <span className="font-medium block mt-2 text-sm cursor-pointer">
+        1k likes
+      </span>
+      <p>
+        <span className="font-medium mr-2">username</span>caption
+      </p>
+      <span>View all 10 comments</span>
+      <CommentDialog />
+      <div className="flex items-center justify-between">
+        <input
+          type="text"
+          placeholder="Add a comment...."
+          value={text}
+          onChange={changeeventHandler}
+          className="outline-none text-sm w-full"
+        />
+        {text && <span className="text-[#3BADF8]">Post</span>}
+      </div>
+    </div>
+  );
+};
+
+export default Post;
